8dc06d51a8b65bbce479bc21538b1213e9026af7
Sergey Pugach <sergey.pugach02@gmail.com>
2022-12-21 22:31:45 +0300

    fixes

--- create.php
+++ create.php
@@ -67,7 +67,7 @@ if(isset($_POST['submit'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Create page</title>
 </head>
 <body class="container">
 <ul class="header container" >
--- delete.php
+++ delete.php
@@ -7,7 +7,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["id"]) && is_numeric($
     try {
         $stmt = $db->prepare($query);
         $stmt->execute([$id]);
-        echo "Record deleted successfully";
+        ?>
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="style.css">
+            <title>Deleted</title>
+        </head>
+
+        <body class="container">
+        <ul class="header container" >
+            <a class="header-item header-item_accent" href="list.php">На главную</a>
+        </ul>
+        <h1>Запись успешно удалена</h1>
+        </body>
+        </html>
+<?php
     } catch(PDOException $e) {
         echo $sql . "<br>" . $e->getMessage();
     }
